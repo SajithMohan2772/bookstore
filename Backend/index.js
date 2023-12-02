49bbb1cfa326c7507ba18e4cf3d3b48d511410a9
SajithMohan2772 <sajith2772@gmail.com>
2023-12-02 21:06:14 +0530

    3rd commit

--- Backend/index.js
+++ Backend/index.js
@@ -105,6 +105,23 @@ app.put('/books/:id', async (request, response) =>{
     }
 });
 
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndUpdate(id);
+
+        if (!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+
+        return response.status(200).send({message: 'Book deleted succesfully'});
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
